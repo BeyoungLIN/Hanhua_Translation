dcffcbbeb01b0bf42c8207708133cd8549689893
BeyoungLIN <BeyoungLIN@users.noreply.github.com>
2020-12-28 14:02:53 +0800

    update files

--- hanArt/utils.py
+++ hanArt/utils.py
@@ -48,7 +48,7 @@ def para2Sentence(para):
 
 
 
-def searchAll(articles,target,searchMethod,threshold=0.9):
+def searchAll(articles,target,searchMethod,threshold=0.9, outputpath=''):
     """
     输入文章集，目标句子，搜索方法和阈值，
     按照指定的搜索方法和阈值在文章集里搜索相似句子，
@@ -57,7 +57,7 @@ def searchAll(articles,target,searchMethod,threshold=0.9):
     """
     sims={}
     # for i,article in enumerate(tqdm(articles)):
-    of = open('/Users/Beyoung/Desktop/研一课件/翻译技术原理/2020级计算机辅助翻译课程竞赛/hanhua_similarsentences.txt', 'a', encoding='utf-8')
+    of = open(outputpath, 'a', encoding='utf-8')
     for i,article in enumerate(articles):
         # print(i, article)
         current_sims=None
@@ -65,19 +65,21 @@ def searchAll(articles,target,searchMethod,threshold=0.9):
         current_sims=searchMethod(article,target,threshold)
         if current_sims:
             sims.update({i:current_sims})
-    if len(sims.values()) >= 20:
+    if len(sims.values()) >= 50:
         print()
         print(target)
         of.writelines('原句：'+target)
         of.writelines('\n')
         print('出现次数：', len(sims.values()))
-        of.writelines('出现次数：'+ str(len(sims.values())))
+        of.writelines('出现次数：'+ str(len(sims.values())-1))
         of.writelines('\n')
         for key, value in sims.items():
-            for value_1 in value:
-                print(value_1)
-                of.writelines(str(value_1))
-                of.writelines('\n')
+            if key != 0:
+                # value_light =
+                for value_1 in value:
+                    print(value_1)
+                    of.writelines(str(value_1))
+                    of.writelines('\n')
         print('*'*20)
         of.writelines('*'*20)
         of.writelines('\n')
@@ -85,7 +87,102 @@ def searchAll(articles,target,searchMethod,threshold=0.9):
 
 
     return sims
-    
+
+
+
+def sentenceParse(sentence):
+    sentences=para2Sentence(sentence)
+    sentencePart=len(sentences)
+    if sentencePart==1:
+        return [sentences]
+    elif sentencePart==2:
+        return [
+            sentences,
+            [sentence]
+        ]
+    elif sentencePart==3:
+        return [
+            sentences,
+            [sentence],
+            [sentences[0],sentences[1]+sentences[2]],
+            [sentences[0]+sentences[1],sentences[2]]
+        ]
+    elif sentencePart==4:
+        return [
+            sentences,
+            [sentence],
+            [sentences[0]+sentences[1],sentences[2],sentences[3]],
+            [sentences[0],sentences[1]+sentences[2],sentences[3]],
+            [sentences[0],sentences[1],sentences[2]+sentences[3]],
+            [sentences[0]+sentences[1]+sentences[2],sentences[3]],
+            [sentences[0],sentences[1]+sentences[2]+sentences[3]]
+        ]
+    elif sentencePart==5:
+        return [
+            sentences,
+            [sentence],
+            [sentences[0]+sentences[1],sentences[2],sentences[3],sentences[4]],
+            [sentences[0],sentences[1]+sentences[2],sentences[3],sentences[4]],
+            [sentences[0],sentences[1],sentences[2]+sentences[3],sentences[4]],
+            [sentences[0],sentences[1],sentences[2],sentences[3]+sentences[4]],
+            [sentences[0]+sentences[1]+sentences[2],sentences[3],sentences[4]],
+            [sentences[0],sentences[1]+sentences[2]+sentences[3],sentences[4]],
+            [sentences[0],sentences[1],sentences[2]+sentences[3]+sentences[4]],
+            [sentences[0]+sentences[1]+sentences[2]+sentences[3],sentences[4]],
+            [sentences[0],sentences[1]+sentences[2]+sentences[3]+sentences[4]]
+        ]
+    else:
+        return [sentences] #如果句子太长，就直接按最小单位拆分并返回
+
+
+
+def dynamicSearch(article,target,threshold=False):
+    """
+    输入文章和目标句子，动态分句子。
+    之后计算每句话和目标句子的相似度，相似度会归一化。
+    默认输出所有句子以及其相似度，如果设置了threshold，只输出相似度>=threshold的句子。
+    """
+
+    global sim
+    result=[]
+
+    bigSentences=para2Sentence_1(article)
+    for sentence in bigSentences:
+        pattern=sentenceParse(sentence)
+        result+=findLocalMax(pattern,target)
+    if threshold!=False:
+        new_result=[]
+        for r in result:
+            if r[1]>=threshold:
+                new_result.append(r)
+        result=new_result
+
+    return result
+
+
+
+def findLocalMax(pattern,target,debug=False):
+    """
+    输入一堆句子，通常是一个句子不同句长的组合。
+    返回包含最大相似句子的那个pattern以及其分句对应概率
+    """
+    probs=[]
+    for p in pattern:
+        probs_temp=[]
+        for parts in p:
+            probs_temp.append(sim.get_score(target,parts))
+        probs.append(probs_temp)
+
+    if debug:
+        return [
+            [pattern[i],probs[i]] for i in range(len(pattern))
+        ]
+    else:
+        probs_max=[max(i) for i in probs]
+        max_prob=max(probs_max)
+        index_max=probs_max.index(max_prob)
+        return [[pattern[index_max][i],c] for i,c in enumerate(probs[index_max])]
+
     
     
 def fixedSearch(article,target,threshold=False):
@@ -118,4 +215,51 @@ def fixedSearch(article,target,threshold=False):
         print(r)
 
     
+    return result
+
+def searchAndShow(article, target, show_all=True, threshold=0.05):
+    """
+
+    """
+    search_sim = text2vec.SearchSimilarity(corpus=article)  # 对每一句话进行相似度计算
+    result = [[element, article[index]] for index, element in enumerate(search_sim.get_scores(query=target))]
+    # print(result)
+    # result = sorted(result, key=lambda x: x[0], reverse=True)  # 按相似度从高到低排序
+
+    if not show_all:
+        result_new = []
+        # temp_sum = 0
+        for r in result:
+            # temp_sum = r[0]
+            if r[0] >= threshold:
+                result_new.append(r)
+        result = result_new
+        # print(result)
+
+    temp_max = [0, '']
+    scoreandsent = []
+    for r in result:
+        if r[0] >= temp_max[0]:
+            temp_max = r
+            # scoreandsent.append(r)
+    for r in result:
+        mark = r[0] / temp_max[0]
+        if mark > 0.8:  # 此处可以调相似度阈值
+            scoreandsent.append(r)
+
+    if len(scoreandsent) > 40:
+        print()
+        print(target)
+        print('最相似句子:', temp_max)
+        print('出现频率', len(scoreandsent))
+        for i in scoreandsent:
+            print(i)
+        print('*' * 20)
+
+    result = scoreandsent
+    # for i, r in enumerate(result):
+    #     print(i,r)
+    #     if i >= threshold:
+    #         result_new.append(r)
+    # result = result_new
     return result
--- sentence_similarity_fixed.py
+++ sentence_similarity_fixed.py
@@ -1,12 +1,11 @@
-import numpy as np
 from tqdm import tqdm
-from hanArt.utils import loadDataFromText
-from hanArt.utils import fixedSearch,searchAll
+
+# from hanArt.utils import loadDataFromText
+from hanArt.utils import searchAll, dynamicSearch
 from utils import *
 
 
-
-def alltxtpieces(filepath):
+def alltxtpieces(filepath, testnum):
     """
     load data from text file, return a list of all sentences of the file
     """
@@ -20,72 +19,24 @@ def alltxtpieces(filepath):
         for j in pieces:
             if j != '':
                 all_text.append(j)
+
+    all_text_new = list(set(all_text[:testnum]))
+    all_text = all_text_new
+
     return all_text
 
 
-all_text = alltxtpieces('./hanhua_jt_fin_test.txt')
+all_text = alltxtpieces('./hanhua_jt_fin_test.txt', 300)
 
 sims = []
 
-
-def searchAndShow(article, target, show_all=True, threshold=0.05):
-    """
-
-    """
-    search_sim = text2vec.SearchSimilarity(corpus=article)  # 对每一句话进行相似度计算
-    result = [[element, article[index]] for index, element in enumerate(search_sim.get_scores(query=target))]
-    # print(result)
-    # result = sorted(result, key=lambda x: x[0], reverse=True)  # 按相似度从高到低排序
-
-    if not show_all:
-        result_new = []
-        # temp_sum = 0
-        for r in result:
-            # temp_sum = r[0]
-            if r[0] >= threshold:
-                result_new.append(r)
-        result = result_new
-        # print(result)
-
-    temp_max = [0, '']
-    scoreandsent = []
-    for r in result:
-        if r[0] >= temp_max[0]:
-            temp_max = r
-            # scoreandsent.append(r)
-    for r in result:
-        mark = r[0]/temp_max[0]
-        if mark > 0.8: # 此处可以调相似度阈值
-            scoreandsent.append(r)
-
-    if len(scoreandsent) > 40:
-        print()
-        print(target)
-        print('最相似句子:', temp_max)
-        print('出现频率', len(scoreandsent))
-        for i in scoreandsent:
-            print(i)
-        print('*' * 20)
-
-    result = scoreandsent
-        # for i, r in enumerate(result):
-        #     print(i,r)
-        #     if i >= threshold:
-        #         result_new.append(r)
-        # result = result_new
-    return result
-
-
-descriptions=loadDataFromText('./hanhua_jt_fin_test.txt')
-# lines = descriptions
+descriptions = loadDataFromText('./hanhua_jt_fin_test.txt')
+outputpath = '/Users/Beyoung/Desktop/研一课件/翻译技术原理/2020级计算机辅助翻译课程竞赛/hanhua_similarsentences_dynamic_95_rmdup_300.txt'
 
 # for para_i in range(50):
-for para_i in tqdm(range(50)):
-    # current_sims = None
+# for para_i in tqdm(range(50)):
+for para_i in tqdm(range(len(all_text))):
     # current_sims = searchAndShow(all_text[para_i + 1:], all_text[para_i], False, 0.05)
     # target='建鼓，上有羽葆，两侧有小鼓' #目标句子
-    # print(all_text[para_i])
-    searchAll(descriptions, all_text[para_i],searchMethod=fixedSearch,threshold=0.9)
+    searchAll(descriptions, all_text[para_i], searchMethod=dynamicSearch, threshold=0.93, outputpath=outputpath)
     # searchAll(descriptions, target,searchMethod=fixedSearch,threshold=0.95)
-    # print(searchAll(descriptions, target,searchMethod=fixedSearch,threshold=0.95))
-
